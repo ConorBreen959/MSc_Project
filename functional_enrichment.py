3ad84d15f44e5398157846fb3891c2c4873c13d6
ConorBreen959 <conorbreen96@gmail.com>
2020-08-19 15:20:55 +0100

    On branch master  Changes to be committed: 	modified:   functional_enrichment.py

--- functional_enrichment.py
+++ functional_enrichment.py
@@ -10,11 +10,11 @@ os.chdir("/home/conor/Documents/Git_Repositories/MSc_Project")
 DEG_list = pd.read_csv('data/DEG_list.csv')
 methyl_genes = pd.read_csv('data/Methylation_genes.csv')
 
-upreg = DEG_list[(DEG_list['adj.P.Val'] <= 0.05) & (DEG_list['logFC'] >= 0.137)]
+upreg = DEG_list[(DEG_list['adj.P.Val'] <= 0.1) & (DEG_list['logFC'] >= 0.1)]
 upreg = upreg['Gene'].astype(str).tolist()
 upreg[:] = map(str.strip,upreg)
 
-downreg = DEG_list[(DEG_list['adj.P.Val'] <= 0.05) & (DEG_list['logFC'] <= -0.137)]
+downreg = DEG_list[(DEG_list['adj.P.Val'] <= 0.1) & (DEG_list['logFC'] <= -0.1)]
 downreg = downreg['Gene'].astype(str).tolist()
 downreg[:] = map(str.strip,downreg)
 
@@ -24,10 +24,14 @@ hyper = hyper['Gene'].tolist()
 hypo = methyl_genes[methyl_genes['Methylation'] < 0]
 hypo = hypo['Gene'].tolist()
 
-genelists = [downreg, upreg, hyper, hypo]
+genelists = {'downreg': downreg, 'upreg': upreg, 'hyper': hyper, 'hypo': hypo}
 
-d = {}
 for i in genelists:
-    d["string{0}".format(i)] = gp.profile(i, organism='mmusculus', significance_threshold_method='fdr', measure_underrepresentation=False, sources=['GO:BP'])
-
-
+    print("Calculating", i, "enrichment...")
+    enrichment = gp.profile(genelists[i], organism='mmusculus', significance_threshold_method='fdr', measure_underrepresentation=False, sources=['GO:BP'])
+    enrichment.sort_values('p_value').to_csv(str(i) + "_enrichment.csv")
+    locals()[str(i) + "_enrichment"] = enrichment
+    print("Done")
+    
+intersection_1 = pd.merge(downreg_enrichment, hyper_enrichment, how = 'inner', on = ['native'])
+intersection_2 = pd.merge(upreg_enrichment, hypo_enrichment, how = 'inner', on = ['native'])
